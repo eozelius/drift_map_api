04f2d58271d60e1642631cb51dc449b03639ef82
ethan ozelius <e.ozelius@gmail.com>
2018-07-20 17:03:00 -0400

    Authentication is not required to view a user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, only: [:update, :destroy]
+  skip_before_action :validate_type, only: [:show]
 
   def show
     user = User.find(params[:id])
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :description
+  attributes :id, :first_name, :last_name, :email, :description, :token
   
   has_one :driftmap
 end
